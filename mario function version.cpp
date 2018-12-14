194d2a343b5f7b3cfda39314f06c6b2c30e33bea
25127高廷睿 <43640542+9487-123@users.noreply.github.com>
2018-12-14 21:35:16 +0800

    Update mario function version.cpp

--- mario function version.cpp
+++ mario function version.cpp
@@ -3,25 +3,21 @@ using namespace std;
 int main() {
 int height;
 do
+{cout << "請輸入三角形高度(1-8):";
+cin >> height;}
+while (height<1 || height>8); 
 {
-cout << "請輸入三角形高度(1-8):";
-cin >> height;
-}
-while (9>height & height>0); {
-for (int a=0;a<height;a++) {
-for (int b=height;b>a;b--) {
-cout << " ";
-}
-for (int b=0;b<=a;b++) {
-cout << "#";
-}
+for (int a=0;a<height;a++)
+{
+for (int b=height;b>a;b--) 
+{cout << " ";}
+for (int b=0;b<=a;b++) 
+{cout << "#";}
 cout << "  ";
-for (int b=0;b<=a;b++) {
-cout << "#";
-}
+for (int b=0;b<=a;b++)
+{cout << "#";}
 cout << "\n";
 }
 cout << "高度:" << height << "\n";
 }
 }
-
